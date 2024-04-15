453a831b7a2fd16641258ef18e5dfb4c88a8cb0e
Siti Rahmah <strahmasti@gmail.com>
2024-04-15 15:31:49 +0800

    add: new routes

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,4 +1,9 @@
 // import HomePage from '../views/Pages/HomePage/HomePage';
+import LoginPage from '../views/Pages/LoginPage/LoginPage';
+import SignupPage from '../views/Pages/LoginPage/SignupPage';
+import Onboarding1Page from '../views/Pages/LoginPage/OnboardingPage1';
+import Onboarding2Page from '../views/Pages/LoginPage/OnboardingPage2';
+
 import HomePage from '../views/Pages/Homepage/HomePage';
 import EventsPage from '../views/Pages/EventsPage/EventsPage';
 import CommunityPage from '../views/Pages/CommunityPage/CommunityPage';
@@ -12,7 +17,11 @@ import FilmPage from '../views/Pages/FilmPage/FilmPage';
 import FilmDetail from '../views/Pages/FilmPage/FilmDetail';
 
 const routes = {
-  '/': HomePage, // default page
+  '/': LoginPage, // default page
+  '/signup': SignupPage,
+  '/onboarding1': Onboarding1Page,
+  '/onboarding2': Onboarding2Page,
+  '/homepage': HomePage,
   '/events': EventsPage,
   '/community': CommunityPage,
   '/volunteer': VolunteerPage,
